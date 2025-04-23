4442699ac50f253385a4475cc49e11fbd8aa583c
YashiKaito <linfengxian79@gmail.com>
2025-04-23 16:46:00 +0900

    Update tweet.py

--- tweet_bot/tweet.py
+++ tweet_bot/tweet.py
@@ -1,12 +1,13 @@
-
 import tweepy
 import os
 
 def post_to_twitter(content):
-    client = tweepy.Client(
-        consumer_key=os.getenv("TWITTER_API_KEY"),
-        consumer_secret=os.getenv("TWITTER_API_SECRET"),
-        access_token=os.getenv("TWITTER_ACCESS_TOKEN"),
-        access_token_secret=os.getenv("TWITTER_ACCESS_SECRET")
+    auth = tweepy.OAuth1UserHandler(
+        os.getenv("TWITTER_API_KEY"),
+        os.getenv("TWITTER_API_SECRET"),
+        os.getenv("TWITTER_ACCESS_TOKEN"),
+        os.getenv("TWITTER_ACCESS_SECRET")
     )
-    client.create_tweet(text=content)
+
+    api = tweepy.API(auth)
+    api.update_status(status=content)
